abf675748d6dee72afcd106cc8befc6795f838c1
lettucejuice04 <christophervu1234@gmail.com>
2026-04-22 23:11:10 -0700

    homework

--- hw3/histogram.py
+++ hw3/histogram.py
@@ -5,6 +5,27 @@ def histogram(points, bins):
     avoid looping through all bins for each point.
 
     """
-    # TODO: Implement the histogram function
+    i = 0
+    count = 0
+    n = len(points)
+    k = len(bins)
+    res = [0.0] * k
+
+    for p in points:
+        # condition here
+        while (i < k and p >= bins[i][1]):
+            left, right = bins[i]
+            res[i] = (count / (n * (right - left)))
+
+            i += 1
+            count = 0
+
+        count+=1
+
+    left, right = bins[i]
+    res[i] = (count / (n * (right - left)))
+
+    return res
+
     
-    return None
+            
--- hw3/swap_sum.py
+++ hw3/swap_sum.py
@@ -6,6 +6,33 @@ def swap_sum(A, B):
     contain integers.
 
     """
-    # TODO: Implement the swap_sum function
-    
+    sum_a = sum(A)
+    sum_b = sum(B)
+
+    # After swapping A[i] and B[j], we need:
+    # (sum_b - B[j] + A[i]) == (sum_a - A[i] + B[j]) + 10
+    # which simplifies to: A[i] - B[j] = (sum_a - sum_b + 10) / 2
+    needed_twice = sum_a - sum_b + 10
+
+    if needed_twice % 2 != 0:
+        return None
+
+    target = needed_twice // 2
+
+    i = 0
+    j = 0
+    len_a = len(A)
+    len_b = len(B)
+
+    while i < len_a and j < len_b:
+        diff = A[i] - B[j]
+
+        if diff == target:
+            return (i, j)
+
+        if diff < target:
+            i += 1
+        else:
+            j += 1
+
     return None
